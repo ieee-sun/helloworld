9f5a63d2fa00754a6b8362bb05cd8237740cef0a
ieee-sun <chungsiusun@gmail.com>
2024-06-07 14:02:22 +0800

    Update test.py
    put in container.  NULL return

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-import time
-
-a = 1
-b = 2
-sum_result = a + b
-
-time.sleep(3)
-
-print(sum_result)
+def hello_add(a, b):
+  
+  import time
+  
+  a = 1
+  b = 2
+  sum_result = a + b
+  
+  time.sleep(3)
+  
+  print(sum_result)
